770dd9a6578e93a9a36f72b420151ce3e932ecb2
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-03-12 19:33:54 -0300

    remove unused components and replace App with Header in main

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return(
+        <main>
+            <section>
+                {/* <img src="" alt="" /> */}
+                <div>
+                    <h3>Antony Rodrigues</h3>
+                    <div>
+                    {/* <a href="http://"><img src="" alt="" srcset="" /></a> */}
+                    {/* <a href="http://"><img src="" alt="" srcset="" /></a> */}
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
