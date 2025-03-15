3705e640145e1c0ddc608072f81e08802e2e4fe9
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-03-15 17:37:07 -0300

    ajusta estilos de texto no cabeçalho, renomeia classes e melhora responsividade

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
             <section className="flex items-center gap-4">
                 <img src="https://github.com/TonyRodIv/portfolio/blob/main/public/pfpG.png?raw=true" className="rounded-full min-w-[60px] w-[15vw] max-w-[90px]" alt="" />
                 <div>
-                    <h3>Antony Rodrigues</h3>
+                    <h3 className="textMedium">Antony Rodrigues</h3>
                     <div className="flex gap-2">
                         <a href="https://github.com/TonyRodIv"><img src="https://github.com/TonyRodIv/portfolio/blob/main/src/assets/icons/github.png?raw=true" alt=""  /></a>
                         <a href="https://www.linkedin.com/in/Tonyrodiv/"><img src="https://github.com/TonyRodIv/portfolio/blob/main/src/assets/icons/linkedin.png?raw=true" alt="" /></a>
@@ -15,7 +15,7 @@ export default function Header() {
                 <h1 className="titleHead font-bold text-[#323232]">
                     Desenvolvo aplicações e transformo suas ideias em soluções digitais
                 </h1>
-                <p className="textHead font-medium text-[#323232cc]">
+                <p className="textSmall font-medium text-[#323232cc]">
                     In a quaint little town, nestled between rolling hills, there lived a curious cat named Whiskers. Every day Whiskers would embark on new adventures, exploring the vibrant gardens and meeting friendly neighbors. One sunny afternoon.
                 </p>
                 <li className="flex items-center"><a href="#contact"><button className="bg-aquaT rounded-2xl py-1.5 px-8 text-gray-50 font-medium text-lg">Contate-me</button></a></li>
