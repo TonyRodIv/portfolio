956ee52e9e85bbbd857f948b9e230d51a8c5ff05
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-03-14 23:44:02 -0300

    adiciona novas imagens e ajusta estilos do cabeçalho e do CSS

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <main className="header h-[90vh] flex flex-col justify-center items-center relative">
             <section>
-                {/* <img src="" alt="" /> */}
+                <img src="" alt="" />
                 <div>
                     <h3>Antony Rodrigues</h3>
                     <div>
@@ -15,12 +15,12 @@ export default function Header() {
                 <h1 className="titleHead font-bold text-[#323232]">
                     Desenvolvo aplicações e transformo suas ideias em soluções digitais
                 </h1>
-                <p className="textHead font-medium text-[#323232cc] text-[22px] leading-normal">
+                <p className="textHead font-medium text-[#323232cc]">
                     In a quaint little town, nestled between rolling hills, there lived a curious cat named Whiskers. Every day Whiskers would embark on new adventures, exploring the vibrant gardens and meeting friendly neighbors. One sunny afternoon.
                 </p>
                 <li className="flex items-center"><a href="#contact"><button className="bg-aquaT rounded-2xl py-1.5 px-8 text-gray-50 font-medium text-lg">Contate-me</button></a></li>
             </main>
-            <img src="https://github.com/TonyRodIv/portfolio/blob/main/public/backgroundPLUS.png?raw=true" className="bottom-20 -right-20 absolute" alt="" />
+            <img src="https://github.com/TonyRodIv/portfolio/blob/main/public/backgroundPLUS.png?raw=true" className="opacity-[40%] bottom-20 -right-20 absolute" alt="" />
         </main>
     )
 }
